73b59ca533e6d0b099a59f009bfa45d94b57e0f1
Sujeban Elankeswaran <silentnerd@users.noreply.github.com>
2019-10-18 00:28:54 +0530

    Updates

---
+++ precache-manifest.aa23f4c2354e686951242a5dd698ab1d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d5593109e185d99f31a18988ef9d311",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ddb28463914403ccb7b",
+    "url": "./static/css/main.d0190f4d.chunk.css"
+  },
+  {
+    "revision": "adc980c02ee28a1ca2d5",
+    "url": "./static/js/2.0c001ddb.chunk.js"
+  },
+  {
+    "revision": "4ddb28463914403ccb7b",
+    "url": "./static/js/main.dab36a13.chunk.js"
+  },
+  {
+    "revision": "769cdfd460361ffa299f",
+    "url": "./static/js/runtime-main.25593f04.js"
+  },
+  {
+    "revision": "a5151bc1b6392768e923241228f7cf24",
+    "url": "./static/media/avatar.a5151bc1.jpg"
+  },
+  {
+    "revision": "ac87480358e62b54d6483050b5219b3a",
+    "url": "./static/media/bell-regular.ac874803.svg"
+  },
+  {
+    "revision": "4647858e9f58b7ff555b42d4b49a62d3",
+    "url": "./static/media/book-read.4647858e.svg"
+  },
+  {
+    "revision": "21c1e3b29181ca902b59240f8e3a4ff6",
+    "url": "./static/media/envelope-regular.21c1e3b2.svg"
+  },
+  {
+    "revision": "fad4640bb7847e3e0c7c21c3837771e5",
+    "url": "./static/media/school.fad4640b.jpg"
+  },
+  {
+    "revision": "13c4defe0388b4c3886bbc1e281fc0f7",
+    "url": "./static/media/stjohns.13c4defe.png"
+  },
+  {
+    "revision": "d4ef9082a30950c4800040be670ec09c",
+    "url": "./static/media/user-graduate-solid.d4ef9082.svg"
+  }
+]);
